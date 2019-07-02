5770b40dd5f681cf156a9eb3e5dc2819f403a9c5
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-07-02 14:43:28 +0200

    copying works

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { readdirSync, mkdir, readFileSync, writeFileSync } = require("fs");
+const { readdirSync, mkdirSync, readFileSync, writeFileSync, copyFileSync, existsSync } = require("fs");
 
 const myArgs = process.argv.slice(2);
 if (myArgs.includes("ls") && myArgs.includes("templates")) {
@@ -19,12 +19,31 @@ if (myArgs[0] === "create") {
   const dest = furtherArgsObj.dest.replace('~', process.env.HOME);
   const { name } = furtherArgsObj;
   const targetDir = `${dest}/${name}`;
-  mkdir(targetDir, { recursive: true }, (err) => {
-    if (err) throw err;
-  });
-  const packageJSONRaw = readFileSync(`${process.env.PWD}/templates/${template}/package.json`, 'utf8');
+  const targetDirArray = targetDir.split("/");
+  for (let i = 2; i <= targetDirArray.length; i++) {
+    let newDir;
+    if (i !== targetDirArray.length) {
+      newDir = targetDirArray.slice(0, i).join("/");
+    } else {
+      newDir = targetDirArray.slice(0).join("/");
+    }
+    const isExisting = existsSync(newDir)
+    if (!isExisting) {
+      mkdirSync(newDir, true);
+    } else {
+      continue;
+    }
+  }
+  const sourceDir = `${process.env.PWD}/templates/${template}`;
+  const packageJSONRaw = readFileSync(`${sourceDir}/package.json`, 'utf8');
   const packageRaw = JSON.parse(packageJSONRaw);
   packageRaw.name = furtherArgsObj.name;
   const packageJSON = JSON.stringify(packageRaw);
   writeFileSync(`${targetDir}/package.json`, packageJSON, { encoding: 'utf8' });
+  const contentsOfTemplate = readdirSync(sourceDir, { encoding: 'utf8' });
+  contentsOfTemplate.forEach(file => {
+    if (!file.includes("package") && file !== 'yarn.lock') {
+      copyFileSync(`${sourceDir}/${file}`, `${targetDir}/${file}`);
+    }
+  });
 }
