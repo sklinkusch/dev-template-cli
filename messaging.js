e1f15f4f81bd207b5a6a6274b8f4972cf152fe0f
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-07-06 13:48:27 +0200

    corrected typo in help message

--- messaging.js
+++ messaging.js
@@ -6,8 +6,8 @@ exports.showHelp = () => {
   The general usage is: node index.js options
   possible options:
   ls templates: list all available templates
-  create <template-name> name="<your project-name>" dest="<destination>":
+  create <template-name> --name="<your project-name>" --dest="<destination>":
   creates a new project folder with the provided name in the destination directory using the given template
   --help: prints this help
   `);
-}
+};
